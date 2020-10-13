2ee3ee3d979880e5e39de72f6b05753237caa714
Hieu Ngo <justacpulover@gmail.com>
2020-10-14 00:54:29 +0200

    Object3D: rotation, position, scale, visibility

--- dist/client/client.js
+++ dist/client/client.js
@@ -5,7 +5,9 @@ import { GUI } from '/jsm/libs/dat.gui.module';
 /* COMPONENTS */
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const axesHelper = new THREE.AxesHelper(100);
+scene.add(axesHelper);
+const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 2;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -19,11 +21,20 @@ const stats = Stats();
 document.body.appendChild(stats.dom);
 const gui = new GUI();
 const cubeFolder = gui.addFolder("Cube");
-cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2, 0.01); // this calls animate()
-cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2, 0.01);
-cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2, 0.01);
-const cameraFolder = gui.addFolder("Camera");
-cameraFolder.add(camera.rotation, 'z', 0, Math.PI * 2, 0.1);
+const cubeRotationFolder = cubeFolder.addFolder("Rotation");
+cubeRotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2, 0.01); // this calls animate()
+cubeRotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2, 0.01);
+cubeRotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2, 0.01);
+const cubePositionFolder = cubeFolder.addFolder("Position");
+cubePositionFolder.add(cube.position, 'x', -10, 10);
+cubePositionFolder.add(cube.position, 'y', -10, 10);
+cubePositionFolder.add(cube.position, 'z', -10, 10);
+const cubeScaleFolder = cubeFolder.addFolder("Scale");
+cubeScaleFolder.add(cube.scale, 'x', 0, 5, 0.1);
+cubeScaleFolder.add(cube.scale, 'y', 0, 5, 0.1);
+cubeScaleFolder.add(cube.scale, 'z', 0, 5, 0.1);
+cubeFolder.add(cube, 'visible', true);
+cubeFolder.open();
 /* EVENTS */
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
