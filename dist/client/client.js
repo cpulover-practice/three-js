cc23086bf8363e1f993df1ebc53ff6dbae1760cb
Hieu Ngo <justacpulover@gmail.com>
2020-10-13 21:50:25 +0200

    Update current data

--- dist/client/client.js
+++ dist/client/client.js
@@ -1,16 +1,18 @@
 import * as THREE from '/build/three.module.js';
 import { OrbitControls } from '/jsm/controls/OrbitControls';
+/* COMPONENTS */
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 2;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-const cube = new THREE.Mesh(geometry, material);
+const boxGeometry = new THREE.BoxGeometry();
+const basicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+const cube = new THREE.Mesh(boxGeometry, basicMaterial);
 scene.add(cube);
-camera.position.z = 2;
+/* EVENTS */
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -18,10 +20,11 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     render();
 }
+/* ANIMATE & RENDER */
 var animate = function () {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
     controls.update();
     render();
 };
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,9 +1,11 @@
 import * as THREE from '/build/three.module.js'
 import { OrbitControls } from '/jsm/controls/OrbitControls'
 
+/* COMPONENTS */
 const scene: THREE.Scene = new THREE.Scene()
 
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 2;
 
 const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -11,14 +13,13 @@ document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
-const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
-const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
+const boxGeometry: THREE.BoxGeometry = new THREE.BoxGeometry()
+const basicMaterial: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
 
-const cube: THREE.Mesh = new THREE.Mesh(geometry, material)
+const cube: THREE.Mesh = new THREE.Mesh(boxGeometry, basicMaterial)
 scene.add(cube)
 
-camera.position.z = 2
-
+/* EVENTS */
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
@@ -27,18 +28,17 @@ function onWindowResize() {
     render()
 }
 
+/* ANIMATE & RENDER */
 var animate = function () {
     requestAnimationFrame(animate)
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
     controls.update()
-
     render()
 };
 
 function render() {
     renderer.render(scene, camera)
 }
+
 animate();
