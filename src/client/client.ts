e5f12abd561c9ed51629b4a9811f87a6e533a978
Hieu Ngo <justacpulover@gmail.com>
2020-10-14 23:03:03 +0200

    MeshBasicMaterial & texture

--- src/client/client.ts
+++ src/client/client.ts
@@ -13,7 +13,7 @@ scene.add(axesHelper)
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 2;
 
-const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
+const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({antialias:true})
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
@@ -23,6 +23,13 @@ const boxGeometry: THREE.BoxGeometry = new THREE.BoxGeometry()
 const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: false })
 const cube: THREE.Mesh = new THREE.Mesh(boxGeometry, material)
 scene.add(cube)
+
+const texture = new THREE.TextureLoader().load("img/grid.png")
+material.map = texture
+const envTexture = new THREE.CubeTextureLoader().load(["img/px_50.png", "img/nx_50.png", "img/py_50.png", "img/ny_50.png", "img/pz_50.png", "img/nz_50.png"])
+//envTexture.mapping = THREE.CubeReflectionMapping
+envTexture.mapping = THREE.CubeRefractionMapping
+material.envMap = envTexture
 
 const stats = Stats()
 document.body.appendChild(stats.dom)
@@ -49,7 +56,12 @@ const options = {
         "FrontSide": THREE.FrontSide,
         "BackSide": THREE.BackSide,
         "DoubleSide": THREE.DoubleSide,
-    }
+    },
+    combine: {
+        "MultiplyOperation": THREE.MultiplyOperation,
+        "MixOperation": THREE.MixOperation,
+        "AddOperation": THREE.AddOperation
+    },
 }
 const materialFolder = gui.addFolder('Material')
 materialFolder.add(material, 'transparent')
@@ -61,9 +73,23 @@ materialFolder.add(material, 'visible')
 materialFolder.add(material, 'side', options.side).onChange(() => updateMaterial())
 materialFolder.open()
 
+var data = {
+    color: material.color.getHex(),
+};
+
+var meshBasicMaterialFolder = gui.addFolder('THREE.MeshBasicMaterial');
+meshBasicMaterialFolder.addColor(data, 'color').onChange(() => { material.color.setHex(Number(data.color.toString().replace('#', '0x'))) });
+meshBasicMaterialFolder.add(material, 'wireframe');
+//meshBasicMaterialFolder.add(material, 'wireframeLinewidth', 0, 10);
+meshBasicMaterialFolder.add(material, 'combine', options.combine).onChange(() => updateMaterial())
+meshBasicMaterialFolder.add(material, 'reflectivity', 0, 1);
+meshBasicMaterialFolder.add(material, 'refractionRatio', 0, 1);
+meshBasicMaterialFolder.open()
+
 function updateMaterial() {
-    // change string ("FrontSide") from dat gui to number to apply change
+    // change string value ("FrontSide") from dat gui to number to apply change
     material.side = Number(material.side)
+    material.combine = Number(material.combine)
     material.needsUpdate = true
 }
 
